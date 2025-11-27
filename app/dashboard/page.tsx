c0a8257c4d354e2ea00ca33e4b9694cc7b1b0eca
Oliver Cecil <ocecil98@gmail.com>
2025-11-27 09:24:34 -0600

    Added redirecting to dashboard if session exists when going to login/register page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { auth, signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 async function Dashboard() {
@@ -13,6 +14,12 @@ async function Dashboard() {
   return (
     <>
       <h1>{session.user.name}</h1>
+      <Image
+        src={!session.user.image ? "" : session.user.image}
+        alt="User Avatar"
+        width={100}
+        height={100}
+      />
       <Button
         onClick={async () => {
           "use server";
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,8 +1,14 @@
+import { auth } from "@/auth";
 import OAuthButtons from "@/components/oauth-buttons";
 import { AuroraText } from "@/components/ui/aurora-text";
 import { MagicCard } from "@/components/ui/magic-card";
+import { redirect } from "next/navigation";
 
-export default function Register() {
+export default async function Register() {
+  const session = await auth();
+
+  if (session?.user) redirect("/dashboard");
+
   return (
     <div className="w-full h-full flex justify-center items-center">
       <MagicCard className="rounded-2xl flex justify-center p-8 pl-16 pr-16">
